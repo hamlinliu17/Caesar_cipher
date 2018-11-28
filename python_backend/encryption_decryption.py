d7130bddba82b14d5b203b7b5a2143a9caaa9a09
Hamlin Liu <hamlin.liu@gmail.com>
2018-11-27 19:46:31 -0800

    shift_letters cannot take negative

--- python_backend/encryption_decryption.py
+++ python_backend/encryption_decryption.py
@@ -12,33 +12,38 @@ def shift_letters(n):
     reverse = alphabet[::-1]
     return alphabet[n:] + reverse[len(alphabet) - n: len(alphabet) + 1]
 
+print(shift_letters(-1))
 
 def encrypt(text, n):
     text = text.upper()
     given_list = list(text)
     alphabet_list = list(alphabet)
+    shifted_alphabet = list(shift_letters(n))
     new_text = []
     for i in given_list:
         if i == ' ':
             new_text += [' ']
         for x in alphabet_list:
             if i == x:
-                new_text += [alphabet_list[alphabet_list.index(x) + n]]
+                new_text += [shifted_alphabet[alphabet_list.index(x)]]
     new_string = ''.join(new_text)
     return new_string
 
-print(encrypt('attack at dawn', 3))
+print(encrypt('attack at dawn', 1))
 
 def decrypt(text, key):
     text = text.upper()
     given_list = list(text)
-    alphabet_list = list(shift_letters(key))
+    alphabet_list = list(alphabet)
+    shifted_alphabet = list(shift_letters(-key))
     new_text = []
     for i in given_list:
         if i == ' ':
             new_text += [' ']
         for x in alphabet_list:
             if i == x:
-                new_text += [alphabet_list[alphabet_list.index(x) - key]]
+                new_text += [shifted_alphabet[alphabet_list.index(x)]]
     new_string = ''.join(new_text)
     return new_string
+
+#print(decrypt('attack at dawn', 1))
